7b3cf85a4753e9c7c1c2c2e11b8b8e7a15ec2920
yota sato <jingangsihaiuei@gmail.com>
2024-05-09 14:48:16 +0900

    国際化されていない部分の修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     registrations:  'users/registrations',
     sessions:       'users/sessions'
   }
-
+  get "/", to: 'books#index'
   resources :books
   resources :users, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
