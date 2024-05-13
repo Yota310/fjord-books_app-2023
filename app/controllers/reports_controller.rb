bd90acd207162cba29e35298390b3ca2bff1c979
yota sato <jingangsihaiuei@gmail.com>
2024-05-13 09:23:08 +0900

    国際化できていなかった部分の修正

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ReportsController < ApplicationController
   before_action :set_report, only: %i[ show edit update destroy ]
 
@@ -34,7 +36,7 @@ class ReportsController < ApplicationController
 
     respond_to do |format|
       if @report.save
-        format.html { redirect_to report_url(@report), notice: "Report was successfully created." }
+        format.html { redirect_to report_url(@report), notice: t('controllers.common.notice_create', name: Report.model_name.human) }
         format.json { render :show, status: :created, location: @report }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -47,7 +49,7 @@ class ReportsController < ApplicationController
   def update
     respond_to do |format|
       if @report.update(report_params)
-        format.html { redirect_to report_url(@report), notice: "Report was successfully updated." }
+        format.html { redirect_to report_url(@report), notice: t('controllers.common.notice_update', name: Report.model_name.human) }
         format.json { render :show, status: :ok, location: @report }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -61,25 +63,26 @@ class ReportsController < ApplicationController
     @report.destroy
 
     respond_to do |format|
-      format.html { redirect_to reports_url, notice: "Report was successfully destroyed." }
+      format.html { redirect_to reports_url, notice: t('controllers.common.notice_destroy', name: Report.model_name.human) }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_report
-      @report = Report.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def report_params
-      params.require(:report).permit(:title, :content, :user_id).merge(user_id: current_user.id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_report
+    @report = Report.find(params[:id])
+  end
 
-    def set_comment_users
-      @users = @comments.map do |comment|
-        User.where("id = #{comment.user_id}")[0]
-      end
+  # Only allow a list of trusted parameters through.
+  def report_params
+    params.require(:report).permit(:title, :content, :user_id).merge(user_id: current_user.id)
+  end
+
+  def set_comment_users
+    @users = @comments.map do |comment|
+      User.where("id = #{comment.user_id}")[0]
     end
+  end
 end
