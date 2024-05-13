46da389557899962f744c34011f0caadffddb894
yota sato <jingangsihaiuei@gmail.com>
2024-05-13 09:50:04 +0900

    rubocopとerblintを使用してコードの修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,11 +3,8 @@
 class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
-    if @comment.save!
-      redirect_back(fallback_location: root_path)
-    else
-      redirect_back(fallback_location: root_path)
-    end
+    @comment.save!
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
