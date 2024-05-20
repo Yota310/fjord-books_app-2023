aceae37ac725f0020ff28fcc44703f496cf9a09d
yota sato <jingangsihaiuei@gmail.com>
2024-05-20 14:31:51 +0900

    カラム名の変更に伴う国際化

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -20,6 +20,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:comment, :commentable_id, :commentable_type, :user_id).merge(user_id: currnet_user.id)
+    params.require(:comment).permit(:content, :commentable_id, :commentable_type, :user_id).merge(user_id: current_user.id)
   end
 end
