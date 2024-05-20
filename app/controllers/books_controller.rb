ebd0e4780e5d8e1689ac9a57627a2b2a517f8ea0
yota sato <jingangsihaiuei@gmail.com>
2024-05-20 09:55:17 +0900

    使用していないjson周りのコードを削除

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,12 +3,10 @@
 class BooksController < ApplicationController
   before_action :set_book, only: %i[show edit update destroy]
 
-  # GET /books or /books.json
   def index
-    @books = Book.order(:id).page(params[:page])
+    @books = Book.order(id: 'ASC').page(params[:page])
   end
 
-  # GET /books/1 or /books/1.json
   def show
     @user = current_user
     @comment = Comment.new
@@ -16,50 +14,40 @@ class BooksController < ApplicationController
     set_comment_users
   end
 
-  # GET /books/new
   def new
     @book = Book.new
     @user = current_user
   end
 
-  # GET /books/1/edit
   def edit; end
 
-  # POST /books or /books.json
   def create
     @book = Book.new(book_params)
 
     respond_to do |format|
       if @book.save
         format.html { redirect_to book_url(@book), notice: t('controllers.common.notice_create', name: Book.model_name.human) }
-        format.json { render :show, status: :created, location: @book }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /books/1 or /books/1.json
   def update
     respond_to do |format|
       if @book.update(book_params)
         format.html { redirect_to book_url(@book), notice: t('controllers.common.notice_update', name: Book.model_name.human) }
-        format.json { render :show, status: :ok, location: @book }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /books/1 or /books/1.json
   def destroy
     @book.destroy
 
     respond_to do |format|
       format.html { redirect_to books_url, notice: t('controllers.common.notice_destroy', name: Book.model_name.human) }
-      format.json { head :no_content }
     end
   end
 
