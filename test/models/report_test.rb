5ec374d66ef93110f919a802ab1058343c271347
yota sato <jingangsihaiuei@gmail.com>
2024-05-22 10:17:44 +0900

    ユーザーと日報のモデルテストの追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,14 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'editable?' do
+    report = reports(:cherry_book)
+    current_user = users(:alice)
+    assert_equal true, report.editable?(current_user)
+  end
+
+  test 'created_on' do
+    report = reports(:cherry_book)
+    assert_equal 'Tue, 21 May 2024 14:33:07.146486000 JST +09:00'.to_date, report.created_on
+  end
 end
