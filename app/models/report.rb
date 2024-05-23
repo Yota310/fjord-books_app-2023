0d6e42942b3a94d305a4630e8e0707b710465cf2
yota sato <jingangsihaiuei@gmail.com>
2024-05-23 17:51:49 +0900

    fatコントローラー解消のための修正

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,15 +3,14 @@
 class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
-  # 自身が引用されたレポートを持っていることをhas_manyで定義
   has_many :mentioned_relation, foreign_key: 'mention_destination_report_id', class_name: 'Mention', dependent: :destroy, inverse_of: :mention_source_report
   has_many :mentioned_reports, through: :mentioned_relation, source: :mention_source_report
-  # 自信を引用したレポートを持っていることをhas_manyで定義
   has_many :mentioning_relation, foreign_key: 'mention_source_report_id', class_name: 'Mention', dependent: :destroy, inverse_of: :mention_destination_report
   has_many :mentioning_reports, through: :mentioning_relation, source: :mention_destination_report
 
   validates :title, presence: true
   validates :content, presence: true
+  after_save :create_mention
 
   def editable?(target_user)
     user == target_user
@@ -19,5 +18,18 @@ class Report < ApplicationRecord
 
   def created_on
     created_at.to_date
+  end
+
+  def create_mention
+    mentioning_reports.destroy_all
+    @urls = content.scan(%r{http://localhost:3000/reports/\d+})
+    return if @urls.nil?
+
+    @mentioned_ids = @urls.map! do |url|
+      url.split('/').last
+    end
+    @mentioned_ids.uniq.each do |mentioned_id| # 言及先の変数
+      Mention.create!(mention_destination_report_id: mentioned_id, mention_source_report_id: id)
+    end
   end
 end
