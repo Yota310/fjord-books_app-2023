5afdfa4408c92312ae02d4a287d88a358ba27dbb
yota sato <jingangsihaiuei@gmail.com>
2024-05-29 16:27:15 +0900

    メンション機能周りのテストを追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -17,4 +17,14 @@ class ReportTest < ActiveSupport::TestCase
     report[:created_at] = 'Tue, 21 May 2024 14:33:07.146486000 JST +09:00'
     assert_equal 'Tue, 21 May 2024 14:33:07.146486000 JST +09:00'.to_date, report.created_on
   end
+
+  test 'save_mentions' do
+    mention_report = reports(:mention_report)
+    mention_report[:content] = 'http://localhost:3000/reports/1'
+    mention_report.save
+    assert_equal 1, Report.find(1).mentioned_reports.count
+    mention_report.update(content: '内容を編集')
+    assert_equal '内容を編集', Report.find(2).content
+    assert_equal 0, Report.find(1).mentioned_reports.count
+  end
 end
