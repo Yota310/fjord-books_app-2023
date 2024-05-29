224ee40c7caa36c962ecf1cb2ff155f6e0835466
yota sato <jingangsihaiuei@gmail.com>
2024-05-29 10:19:44 +0900

    reportモデルのテストの考慮漏れを修正

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -7,10 +7,14 @@ class ReportTest < ActiveSupport::TestCase
     report = reports(:cherry_book)
     current_user = users(:alice)
     assert_equal true, report.editable?(current_user)
+
+    another_user = users(:bob)
+    assert_equal false, report.editable?(another_user)
   end
 
   test 'created_on' do
     report = reports(:cherry_book)
+    report[:created_at] = 'Tue, 21 May 2024 14:33:07.146486000 JST +09:00'
     assert_equal 'Tue, 21 May 2024 14:33:07.146486000 JST +09:00'.to_date, report.created_on
   end
 end
