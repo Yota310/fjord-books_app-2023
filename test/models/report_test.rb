a50a3634d8e7c25cadb00acdb4a195958f9b0423
yota sato <jingangsihaiuei@gmail.com>
2024-05-31 10:34:18 +0900

    考慮漏れのテストケースを追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -20,11 +20,30 @@ class ReportTest < ActiveSupport::TestCase
 
   test 'save_mentions' do
     mention_report = reports(:mention_report)
+    another_mention_report = reports(:another_mention_report)
     mention_report[:content] = 'http://localhost:3000/reports/1'
+
     mention_report.save
     assert_equal 1, Report.find(1).mentioned_reports.count
+    another_mention_report[:content] = 'http://localhost:3000/reports/2'
+    another_mention_report.save
+    assert_equal 1, Report.find(1).mentioned_reports.count
+    assert_equal 1, Report.find(2).mentioned_reports.count
+
     mention_report.update(content: '内容を編集')
     assert_equal '内容を編集', Report.find(2).content
     assert_equal 0, Report.find(1).mentioned_reports.count
+
+    mention_to_id2_report1 = reports(:mention_to_id2_report1)
+    mention_to_id2_report2 = reports(:mention_to_id2_report2)
+    mention_to_id2_report1[:content] = 'http://localhost:3000/reports/2'
+    mention_to_id2_report2[:content] = 'http://localhost:3000/reports/2'
+    mention_to_id2_report1.save
+    mention_to_id2_report2.save
+    assert_equal 3, Report.find(2).mentioned_reports.count
+
+    self_mention_report = reports(:self_mention_report)
+    self_mention_report[:content] = 'http://localhost:3000/reports/6'
+    assert_equal 0, Report.find(6).mentioned_reports.count
   end
 end
