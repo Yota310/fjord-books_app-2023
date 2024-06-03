82754ff3eedc947924ac2de4091ebcf0a3288332
yota sato <jingangsihaiuei@gmail.com>
2024-06-03 11:50:25 +0900

    テスト内容の修正

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -19,31 +19,31 @@ class ReportTest < ActiveSupport::TestCase
   end
 
   test 'save_mentions' do
+    cherry_book = reports(:cherry_book)
     mention_report = reports(:mention_report)
     another_mention_report = reports(:another_mention_report)
-    mention_report[:content] = 'http://localhost:3000/reports/1'
+    mention_report.content = "私はhttp://localhost:3000/reports/#{cherry_book.id}のレポートについて言及します"
 
     mention_report.save
-    assert_equal 1, Report.find(1).mentioned_reports.count
-    another_mention_report[:content] = 'http://localhost:3000/reports/2'
+    assert_equal 1, mention_report.mentioning_reports.count
+    another_mention_report.content = "私はhttp://localhost:3000/reports/#{mention_report.id}のレポートについて言及します"
     another_mention_report.save
-    assert_equal 1, Report.find(1).mentioned_reports.count
-    assert_equal 1, Report.find(2).mentioned_reports.count
+    assert_equal cherry_book.title, mention_report.mentioning_reports[0].title
+    assert_equal mention_report.title, another_mention_report.mentioning_reports[0].title
 
     mention_report.update(content: '内容を編集')
     assert_equal '内容を編集', Report.find(2).content
     assert_equal 0, Report.find(1).mentioned_reports.count
 
-    mention_to_id2_report1 = reports(:mention_to_id2_report1)
-    mention_to_id2_report2 = reports(:mention_to_id2_report2)
-    mention_to_id2_report1[:content] = 'http://localhost:3000/reports/2'
-    mention_to_id2_report2[:content] = 'http://localhost:3000/reports/2'
-    mention_to_id2_report1.save
-    mention_to_id2_report2.save
-    assert_equal 3, Report.find(2).mentioned_reports.count
+    mentioned_double_report = reports(:mentioned_double_report)
+    mention_double_report = reports(:mention_double_report)
+    mention_double_report.content = "私はこのレポートについて2重で言及しますhttp://localhost:3000/reports/#{mentioned_double_report.id}、http://localhost:3000/reports/#{mentioned_double_report.id}"
+    mention_double_report.save
+    assert_equal 1, mention_double_report.mentioning_reports.count
+    assert_equal mentioned_double_report.title, mention_double_report.mentioning_reports[0].title
 
     self_mention_report = reports(:self_mention_report)
-    self_mention_report[:content] = 'http://localhost:3000/reports/6'
+    self_mention_report.content = "自らに言及をしますhttp://localhost:3000/reports/#{self_mention_report.id}"
     assert_equal 0, Report.find(6).mentioned_reports.count
   end
 end
