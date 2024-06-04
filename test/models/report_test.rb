5def2c7bf435794ac2b30ff9e45a0d18adaaebe5
yota sato <jingangsihaiuei@gmail.com>
2024-06-04 10:29:21 +0900

    テストの中身を整理

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -15,25 +15,26 @@ class ReportTest < ActiveSupport::TestCase
   test 'created_on' do
     report = reports(:cherry_book)
     report[:created_at] = 'Tue, 21 May 2024 14:33:07.146486000 JST +09:00'
-    assert_equal 'Tue, 21 May 2024 14:33:07.146486000 JST +09:00'.to_date, report.created_on
+    assert_equal Date.new(2024, 5, 21), report.created_on
   end
 
   test 'save_mentions' do
     cherry_book = reports(:cherry_book)
     mention_report = reports(:mention_report)
-    another_mention_report = reports(:another_mention_report)
     mention_report.content = "私はhttp://localhost:3000/reports/#{cherry_book.id}のレポートについて言及します"
-
     mention_report.save
     assert_equal 1, mention_report.mentioning_reports.count
+    assert_equal cherry_book.title, mention_report.mentioning_reports[0].title
+
+    another_mention_report = reports(:another_mention_report)
     another_mention_report.content = "私はhttp://localhost:3000/reports/#{mention_report.id}のレポートについて言及します"
     another_mention_report.save
-    assert_equal cherry_book.title, mention_report.mentioning_reports[0].title
+    assert_equal 1, another_mention_report.mentioning_reports.count
     assert_equal mention_report.title, another_mention_report.mentioning_reports[0].title
 
     mention_report.update(content: '内容を編集')
-    assert_equal '内容を編集', Report.find(2).content
-    assert_equal 0, Report.find(1).mentioned_reports.count
+    assert_equal '内容を編集', Report.find(mention_report.id).content
+    assert_equal 0, Report.find(cherry_book.id).mentioned_reports.count
 
     mentioned_double_report = reports(:mentioned_double_report)
     mention_double_report = reports(:mention_double_report)
@@ -44,6 +45,6 @@ class ReportTest < ActiveSupport::TestCase
 
     self_mention_report = reports(:self_mention_report)
     self_mention_report.content = "自らに言及をしますhttp://localhost:3000/reports/#{self_mention_report.id}"
-    assert_equal 0, Report.find(6).mentioned_reports.count
+    assert_equal 0, Report.find(self_mention_report.id).mentioned_reports.count
   end
 end
