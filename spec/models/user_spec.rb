e937dcfebdb0dfe6814dd6a0dc6330aa15e596ee
yota sato <jingangsihaiuei@gmail.com>
2024-06-24 20:55:44 +0900

    ユーザーモデルのテストを追加

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe '#name_or_email' do
+    let!(:user) { User.new(id: 1, email: 'alice@example.com', name: '') }
+    it 'return email. if name does not exist' do
+      expect(user.name_or_email).to eq 'alice@example.com'
+    end
+    it 'return name. if name exist' do
+      user.update(name: 'alice')
+      expect(user.name_or_email).to eq 'alice'
+    end
+  end
+end
