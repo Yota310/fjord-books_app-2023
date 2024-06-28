4ef45ea98804e0d8e13533197880de440a04d102
yota sato <jingangsihaiuei@gmail.com>
2024-06-28 13:01:27 +0900

    returnとifの組み合わせをifのみに修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -7,8 +7,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   def update_resource(resource, params)
-    return super if params['password'].present?
-
-    resource.update_without_password(params.except('current_password'))
+    if params['password'].present?
+      super
+    else
+      resource.update_without_password(params.except('current_password'))
+    end
   end
 end
