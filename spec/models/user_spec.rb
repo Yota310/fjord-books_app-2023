b803688f436986f75c22c4df480ebac9ad27b3b6
yota sato <jingangsihaiuei@gmail.com>
2024-07-01 16:05:46 +0900

    英語を日本語に統一する修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,12 +5,16 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe '#name_or_email' do
     let(:user) { User.new(email: 'alice@example.com', name: '') }
-    it 'return email. if name does not exist' do
-      expect(user.name_or_email).to eq 'alice@example.com'
+    context '名前が存在しない時' do
+      it 'Eメールを返す' do
+        expect(user.name_or_email).to eq 'alice@example.com'
+      end
     end
-    it 'return name. if name exist' do
-      user.update!(name: 'alice')
-      expect(user.name_or_email).to eq 'alice'
+    context '名前が存在する時' do
+      it '名前を返す' do
+        user.update!(name: 'alice')
+        expect(user.name_or_email).to eq 'alice'
+      end
     end
   end
 end
