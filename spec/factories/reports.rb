c7f300ea9d55bc66332437ea1898d0788e47c67c
yota sato <jingangsihaiuei@gmail.com>
2024-07-02 16:52:05 +0900

    reportモデルのファクトリーを追加

---
+++ spec/factories/reports.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :report do
+    title { '今日のレポート' }
+    content { 'aliceの日報です、日報を書きます。それぞれの機能のテストのために書きました。' }
+    sequence(:id) { |n| n }
+    association :user
+
+    factory :mentioned_report do
+      title { 'メンションされるレポート' }
+      content { 'この日報はメンションされる日報になります。' }
+    end
+
+    factory :mention_report do
+      title { 'メンションするレポート' }
+    end
+  end
+end
