e135900279b04921d66eaf7ec32f8d62dcbe8485
yota sato <jingangsihaiuei@gmail.com>
2024-07-03 19:40:18 +0900

    不要なnull:falseとdefaultを削除

--- db/migrate/20240618072403_add_omniauth_columns_to_users.rb
+++ db/migrate/20240618072403_add_omniauth_columns_to_users.rb
@@ -1,7 +1,7 @@
 class AddOmniauthColumnsToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :uid,      :string, null: false, default: ""
-    add_column :users, :provider, :string, null: false, default: ""
+    add_column :users, :uid, :string
+    add_column :users, :provider, :string
  
     add_index :users, [:uid, :provider], unique: true
   end
