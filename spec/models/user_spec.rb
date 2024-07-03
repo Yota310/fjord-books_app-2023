075d26dbf682cbe2c042272381ef5d69dca4fac1
yota sato <jingangsihaiuei@gmail.com>
2024-07-03 09:49:06 +0900

    eメールの呼び出し方法を修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,7 +6,8 @@ RSpec.describe User, type: :model do
   describe '#name_or_email' do
     context '名前が存在しない時' do
       it 'Eメールを返す' do
-        expect(FactoryBot.build(:user, name: nil).name_or_email).to eq 'alice22@example.com'
+        user = FactoryBot.build(:user, name: nil)
+        expect(user.name_or_email).to eq user.email
       end
     end
     context '名前が存在する時' do
