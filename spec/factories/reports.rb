9bab33c84748c87ff593dc40c0e42830c8568119
yota sato <jingangsihaiuei@gmail.com>
2024-07-12 18:09:54 +0900

    余分なreportのファクトリーを削除

--- spec/factories/reports.rb
+++ spec/factories/reports.rb
@@ -5,23 +5,5 @@ FactoryBot.define do
     title { '今日のレポート' }
     content { 'aliceの日報です、日報を書きます。それぞれの機能のテストのために書きました。' }
     association :user
-
-    factory :mentioned_report do
-      title { 'メンションされるレポート' }
-      content { 'この日報はメンションされる日報になります。' }
-    end
-
-    factory :mention_report do
-      title { 'メンションするレポート' }
-    end
-
-    factory :lost_mentioned_report do
-      title { 'メンションされなくなるレポート' }
-      content { 'この日報はメンションされる日報になります。' }
-    end
-
-    factory :add_mentioned_report do
-      title { '後からメンションをするレポート' }
-    end
   end
 end
