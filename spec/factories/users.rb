368c4b70cda071a1c3bea1e769e8b93e8cf9ba94
yota sato <jingangsihaiuei@gmail.com>
2024-07-12 18:23:17 +0900

    userファクトリーの避けな部分を削除

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,9 +6,5 @@ FactoryBot.define do
     sequence(:email) { |n| "alice#{n}@example.com" }
     password { '123456' }
     password_confirmation { '123456' }
-
-    factory :another_user do
-      name { 'bob' }
-    end
   end
 end
