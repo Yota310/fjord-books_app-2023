f94cf0ebf5d08a075441852d194b2ba3a0f75a7b
mpg-yota-sato <yota.sato@fitsplus.co.jp>
2024-07-30 17:56:21 +0900

    userをletで定義する修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,14 +5,16 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe '#name_or_email' do
     context '名前が存在しない時' do
-      user = FactoryBot.build(:user, name: nil)
+      let(:user) { FactoryBot.build(:user, name: nil) }
+
       it 'Eメールを返す' do
         expect(user.name_or_email).to eq user.email
       end
     end
 
     context '名前が存在する時' do
-      user = FactoryBot.build(:user, name: 'alice')
+      let(:user) { FactoryBot.build(:user, name: 'alice') }
+      
       it '名前を返す' do
         expect(user.name_or_email).to eq 'alice'
       end
